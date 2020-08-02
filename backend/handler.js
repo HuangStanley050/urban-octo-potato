f53c7ad4cb1e86dfb035b7ba9e3b5a7db8c73b2d
Stanley Huang <infamous_godhand@yahoo.com>
2020-08-02 12:33:12 +1000

    able to parse a file upload from postman

--- backend/handler.js
+++ backend/handler.js
@@ -1,14 +1,26 @@
-export const hello = async (event, context) => {
+import fs from "fs";
+import { parse } from "aws-lambda-multipart-parser";
+
+export const encrypt = async (event, context) => {
+  const writeStream = fs.createWriteStream("/tmp/experiment");
+  //console.log(event);
+  const result = parse(event, false);
+  console.log(result);
+  result.file.content.pipe(writeStream);
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
+      message: `Go Serverless v1.0! ${await message({
+        time: 1,
+        copy: "Your function executed successfully!",
+      })}`,
     }),
   };
 };
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+const message = ({ time, ...rest }) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(`${rest.copy} (with a delay)`);
+    }, time * 1000)
+  );
